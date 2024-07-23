a0d6e530ba98728846452ac7b15941c4e35bbc2c
dj <dj@curatest.de>
2024-07-23 15:35:19 +0200

    v3.2.1

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'curasoft/api.curasoft',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'd0ac14d5e8758100f8145bc6cba853ba3c43676b',
+        'reference' => '821cba78f75290a367cb38bda64bc1989b19fda3',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'curasoft/api.curasoft' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'd0ac14d5e8758100f8145bc6cba853ba3c43676b',
+            'reference' => '821cba78f75290a367cb38bda64bc1989b19fda3',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
