6403bedef86426724c281440a07790df6aa8fcb4
dj <dj@curatest.de>
2024-07-30 12:13:00 +0200

    hotfix v8.2

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'curasoft/api.curasoft',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '0a65289e06b3322fcdf8642d9c8cfd67dbfb4f75',
+        'reference' => '7d051011b02bab341567bbee3fc6f8e567bdbc90',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'curasoft/api.curasoft' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '0a65289e06b3322fcdf8642d9c8cfd67dbfb4f75',
+            'reference' => '7d051011b02bab341567bbee3fc6f8e567bdbc90',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
